4855a025c128b5a59bffd5c3dba1fae5bee6506a
meghapatil9096 <meghapatil9096@gmail.com>
2025-02-19 17:36:31 +0530

    First commit

--- src/main/CListDemo.java
+++ src/main/CListDemo.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 class StringLengthComparator implements Comparator<String>
 {
-
     @Override
     public int compare(String o1, String o2) {
         return o1.length() - o2.length();
