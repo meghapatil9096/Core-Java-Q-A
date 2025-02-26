c68733777123d1c63886cb73e22649c0e902f331
meghapatil9096 <meghapatil9096@gmail.com>
2025-02-26 13:06:42 +0530

    First commit

---
+++ src/Demo/InstanceVariableDemo.java
@@ -0,0 +1,18 @@
+package Demo;
+class A2
+{
+//  Instance Variable
+    int a=23,b=12;
+    void put()
+    {
+        int temp=a+b;   //Local Variable
+        System.out.println("Sum of two number is "+temp);
+    }
+}
+public class InstanceVariableDemo {
+    public static void main(String[] args) {
+        A2 a=new A2();
+        a.put();
+        System.out.println(a.a);
+    }
+}
---
+++ src/Demo/LocalVariableDemo.java
@@ -0,0 +1,17 @@
+package Demo;
+
+public class LocalVariableDemo {
+
+    void put(int temp)   //parameter temp also local variable
+    {
+        //local variable
+        int a=23,b=12;
+         temp=a+b;
+        System.out.println("Sum of two number is "+temp);
+    }
+    public static void main(String[] args) {
+            LocalVariableDemo obj = new LocalVariableDemo();
+            int t = 0;
+            obj.put(t);
+    }
+}
