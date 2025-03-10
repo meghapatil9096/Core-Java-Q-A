5da68146436522c0db3e98aad40816ea17ad556f
meghapatil9096 <meghapatil9096@gmail.com>
2025-03-10 12:16:16 +0530

    new folders

---
+++ Java-8-Features/src/com/neosoft/Functinal_InterfaceDemo/Function_InterfaceDemo1.java
@@ -0,0 +1,11 @@
+package com.neosoft.Functinal_InterfaceDemo;
+
+interface Calculation{
+    int add(int a, int b);  //Function Interface with single or only one abstract method
+}
+public class Function_InterfaceDemo1 {
+    public static void main(String[] args) {
+        Calculation calc = (a,b)-> a+b; //Lambda Expression () -> Parameters/Arguments
+        System.out.println("Sum : "+calc.add(4,5));
+    }
+}
---
+++ Java-8-Features/src/com/neosoft/Functinal_InterfaceDemo/Function_InterfaceDemo2.java
@@ -0,0 +1,18 @@
+package com.neosoft.Functinal_InterfaceDemo;
+
+interface Square{
+    int sqr(int x); //single abstract method
+}
+
+public class Function_InterfaceDemo2 {
+    public static void main(String[] args) {
+        int a=5;
+
+        Square s = (x)-> x * x;
+
+//        int ans = s.sqr(a);
+//        System.out.println("Square of X is : "+ans);
+
+        System.out.println("Square of X is : "+s.sqr(a));
+    }
+}
